75dd57b46f2b1d650aa4f58c4e5328211b754368
Savannah Kolb <savannahnkolb@gmail.com>
2021-08-14 20:22:25 -0400

    starter code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import Card from './components/Card';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
-function App() {
-  return (
-    <div>
-      <Header />
-      <Navbar />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
