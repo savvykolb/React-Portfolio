6b8138e8b4a5a04b3820ee3e17dc681c48864f41
Savannah Kolb <savannahnkolb@gmail.com>
2021-08-14 21:46:23 -0400

    added fake header and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <div>
+        <h5>I am a Footer!</h5>
+      </div>
+    );
+  }
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -5,10 +5,12 @@ import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
+
 export default function PortfolioContainer() {
+
   const [currentPage, setCurrentPage] = useState('About');
 
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+
   const renderPage = () => {
     if (currentPage === 'About') {
       return <About />;
@@ -26,9 +28,7 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      {/* We are passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
     </div>
   );
