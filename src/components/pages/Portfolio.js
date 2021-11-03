c216166cea5fc4ef2133e053a5b10482fd0d1496
Savannah Kolb <savannahnkolb@gmail.com>
2021-11-02 20:24:43 -0400

    updated portfolio section with ZEAL

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,10 +1,11 @@
 import '../styles/Style.css';
+import zeal from '../../img/portfolio/ZEAL.jpg'
 import jetAway from '../../img/portfolio/JetAway.jpg'
 import foodApedia from '../../img/portfolio/foodImg.jpg'
 import weather from '../../img/portfolio/weather.jpg'
 import Notes from '../../img/portfolio/Notes.jpg'
 import scheduler from '../../img/portfolio/planner.jpg'
-import trivia from '../../img/portfolio/quiz.jpg'
+// import trivia from '../../img/portfolio/quiz.jpg'
 
 import { FaGithub, FaLink } from 'react-icons/fa';
 
@@ -22,16 +23,26 @@ function Portfolio() {
 
         <div class="row portfolio-container">
 
+        <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <div class="portfolio-wrap">
+              <img src={zeal} class="img-fluid" alt="Photographer: John Schnobrich"></img>
+              <div class="portfolio-info">
+                <h4>ZEAL</h4>
+                <p>React / Node.js / MongoDB / GraphQL</p>
+                <a target='_blank' rel="noreferrer" href="https://snk923.github.io/React-Portfolio/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/snk923/ZEAL"><FaGithub /></a>
+              </div>
+            </div>
+          </div>
+
           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
             <div class="portfolio-wrap">
               <img src={jetAway} class="img-fluid" alt="Photographer: Boxed Water"></img>
               <div class="portfolio-info">
                 <h4>JetAway</h4>
-                <p>HTML/CSS/JavaScript / Node.js / mySQL</p>
+                <p>HTML / CSS / JavaScript / Node.js / mySQL</p>
                 <a target='_blank' rel="noreferrer" href="https://jetaway1.herokuapp.com/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/snk923/JetAway"><FaGithub /></a>
               </div>
             </div>
-
           </div>
 
           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
@@ -80,7 +91,7 @@ function Portfolio() {
             </div>
           </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+          {/* <div class="col-lg-4 col-md-6 portfolio-item filter-card">
             <div class="portfolio-wrap">
               <img src={trivia} class="img-fluid" alt="Trivia Game Photographer Emma Matthews Digital Content"></img>
               <div class="portfolio-info">
@@ -89,7 +100,7 @@ function Portfolio() {
                 <a target='_blank' rel="noreferrer" href="https://snk923.github.io/homework4_code_quiz/"><FaLink /></a><a target='_blank' rel="noreferrer" href="https://github.com/snk923/homework4_code_quiz"><FaGithub /></a>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
