9296ee33cfe01f07351c59cf89e14642601514eb
Savannah Kolb <savannahnkolb@gmail.com>
2022-01-22 14:02:21 -0500

    updated contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -46,6 +46,7 @@ const Contact = () => {
           <h2>Contact</h2>
           <p>Interested in working together? Let's connect and create something cool! </p>
           <br></br>
+          <br></br>
           <div class="social-links">
           <a target='_blank' rel="noreferrer" href="tel:336-707-0007" class="phoneNumber"><BsPhone /></a>
           <a target='_blank' rel="noreferrer" href="mailto:savannahnkolb@gmail.com" class="email"><BsFillEnvelopeOpenFill /></a>
